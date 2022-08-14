4732f99621f4bfd6538708b68672fbaebf455879
10Qu <109066870+10Qu@users.noreply.github.com>
2022-08-14 11:46:45 +0200

    Add Files via upload
    - Guessgame1 - madlibs1 - story1

---
+++ madlibs/Guess 1/guessgame1.py
@@ -0,0 +1,76 @@
+# GuessGame No.1
+
+# Strings
+
+from tkinter.messagebox import QUESTION
+
+Rules = "Fill the blank spaces to guess the word! Have Fun!"
+Ready = "Are you ready? y/n \n"
+wrongA = "Invalid Input, Try again!"
+
+#q1 = "a"
+#q2 = "a"
+#q3 = "a"
+#q4 = "a"
+#q5 = "a"
+
+# Rules
+
+print(Rules)
+
+# understood? Type something
+def tutorial():
+    question = (Ready)
+    
+tutorial()
+# Question 1
+
+# Take a guess
+
+# wrong? 
+# Loop to Question 1
+
+# right? 
+# Question 2
+
+# Take a guess
+
+# wrong? 
+# Loop to Question 2
+
+# right? 
+# Question 3
+
+# Take a guess
+
+# wrong? 
+# Loop to Question 3
+
+# right? 
+# Question 3
+
+# Take a guess
+
+# wrong? 
+# Loop to Question 3
+
+# right? 
+# Question 4
+
+# Take a guess
+
+# wrong? 
+# Loop to Question 4
+
+# right? 
+# Question 5
+
+# Take a guess
+
+# wrong? 
+# Loop to Question 5
+
+# right? 
+# Everything done!
+
+
---
+++ madlibs/Story 1/story1.py
@@ -0,0 +1,35 @@
+# Strings
+adj1 = input("Adjektive: ")
+adj2 = input("Adjektive: ")
+transport1 = input("TransportVehicle: ")
+land1 = input("Country: ")
+taste1 = input("Taste: ")
+taste2 = input("Taste: ")
+food1 = input("Food: ")
+verb1 = input("Verb: ")
+
+# Story
+madlib = f"The Perfect Adventure! \
+There once was a fox whose best friend was a {adj1} llama. For llama`s birthday, fox \
+decided to take their friend on a {adj2} adventrue to celebrate their great love for eacht other. \
+So, knowing not where to begin, they started by using the {transport1}. Scoured {land1} to find an \
+adventure. They advanced until they found the {taste1} land of {food1}. \
+\
+In the land lived {taste2} creatures whose job it was to {verb1} all day long. \
+They approached one of the creatures, and asked their recommendation for a {adj2} adventure. \
+The creatures answered, ``{taste1} Morning! \
+\
+That`s easy. Simply build something__ that`ll make a lasting impact.`` \
+So they set off to __ and together built a tiny __ with giant __. Upon their \
+return, they sought a second opinion from a __ creature, who advised, \
+``Do something that changes you life and the way you __ the world.`` \
+So the friends decided to take a __ fling lesson and discovered all kinds of __ wonders.\
+\
+At this point, the sund was setting on this rather __ day. Llama fell asleep with a happy __ \
+on their face, while fox worried over their __, wasted hours. Just then the fox caught sight \
+of llama, whose hands hel a __ journal entry detailing their adventuers. \
+That`s when fox realized __ that wherever they were together was a __ adventure. \
+The Fox settled in to their friend, placed their __ round them, \
+and sighed the most contented __ she`d ever sighed."
+
+print(madlib)
